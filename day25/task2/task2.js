90ab5dbc62678680f6da07e809bd4e50e371d678
Divya-John <divya.john@vonnue.com>
2025-11-22 14:47:08 +0530

    day27

--- day25/task2/task2.js
+++ day25/task2/task2.js
@@ -1,35 +1,86 @@
+var __spreadArray = (this && this.__spreadArray) || function (to, from, pack) {
+    if (pack || arguments.length === 2) for (var i = 0, l = from.length, ar; i < l; i++) {
+        if (ar || !(i in from)) {
+            if (!ar) ar = Array.prototype.slice.call(from, 0, i);
+            ar[i] = from[i];
+        }
+    }
+    return to.concat(ar || Array.prototype.slice.call(from));
+};
 var container = document.getElementById("container");
 var prevBtn = document.getElementById("prev");
 var nextBtn = document.getElementById("next");
-var images = ["imgs/image4.png", "imgs/image1.png", "imgs/image2.png"];
-var currentImage = 0;
-var image = document.createElement("img");
-image.style.padding = "20px";
-const animations = {
-  duration: 1500, 
-  easing: "ease-out",
-};
+var originalImages = [
+    "imgs/image4.png",
+    "imgs/image1.png",
+    "imgs/image2.png",
+];
+var images = __spreadArray(__spreadArray([
+    originalImages[originalImages.length - 1]
+], originalImages, true), [
+    originalImages[0],
+], false);
+var currentImage = 1;
+var interval;
+var isAnimating = false;
+var divcontainer = document.createElement("div");
+divcontainer.className = "slider";
+divcontainer.style.flexShrink = "0";
+container.appendChild(divcontainer);
+images.forEach(function (item) {
+    var image = document.createElement("img");
+    image.src = item;
+    divcontainer.appendChild(image);
+});
 function newImg() {
-  image.src = images[currentImage];
-  image.animate([{ opacity: 0.4 }, { opacity: 1 }], animations);
+    divcontainer.style.transition = "transform 0.5s ease-in-out";
+    divcontainer.style.transform = "translateX(-".concat(currentImage * 500, "px)");
 }
-container.appendChild(image);
-
-prevBtn.addEventListener("click", prevs);
 function prevs() {
-  currentImage--;
-  if (currentImage < 0) {
-    currentImage = images.length - 1;
-  }
-  newImg();
+    if (isAnimating)
+        return;
+    isAnimating = true;
+    currentImage--;
+    if (currentImage < 0) {
+        currentImage = originalImages.length - 1;
+    }
+    newImg();
 }
-nextBtn.addEventListener("click", next);
 function next() {
-  currentImage++;
-  if (currentImage >= images.length) {
-    currentImage = 0;
-  }
-  newImg();
+    if (isAnimating)
+        return;
+    isAnimating = true;
+    currentImage++;
+    newImg();
 }
-setInterval(next, 3000);
+divcontainer.addEventListener("transitionend", function () {
+    isAnimating = false;
+    if (currentImage === images.length - 1) {
+        divcontainer.style.transition = "none";
+        currentImage = 1;
+        divcontainer.style.transform = "translateX(-".concat(currentImage * 500, "px)");
+    }
+    if (currentImage === 0) {
+        divcontainer.style.transition = "none";
+        currentImage = originalImages.length;
+        divcontainer.style.transform = "translateX(-".concat(currentImage * 500, "px)");
+    }
+});
 newImg();
+function pause() {
+    interval = setInterval(next, 3000);
+}
+function nopause() {
+    if (interval !== undefined) {
+        clearInterval(interval);
+    }
+}
+pause();
+divcontainer.addEventListener("mouseover", nopause);
+divcontainer.addEventListener("mouseout", pause);
+if (nextBtn) {
+    nextBtn.addEventListener("click", next);
+}
+if (prevBtn) {
+    prevBtn.addEventListener("click", prevs);
+}
--- day25/task2/task2.ts
+++ day25/task2/task2.ts
@@ -2,55 +2,80 @@ const container = document.getElementById("container") as HTMLDivElement;
 const prevBtn = document.getElementById("prev") as HTMLButtonElement;
 const nextBtn = document.getElementById("next") as HTMLButtonElement;
 
-const images = [
+const originalImages = [
   "imgs/image4.png",
   "imgs/image1.png",
   "imgs/image2.png",
 ];
-let currentImage = 0;
-const stylediv=document.createElement("div")
-const image = document.createElement("img");
-image.style.padding = "20px";
-image.style.animationName="slid";
-image.style.animationDuration="1.5s";
+const images = [
+  originalImages[originalImages.length - 1],
+  ...originalImages,
+  originalImages[0],
+];
+let currentImage = 1;
+let interval: number | undefined;
+
+let isAnimating = false;
+
+let divcontainer = document.createElement("div");
+divcontainer.className = "slider";
+divcontainer.style.flexShrink="0"
+container.appendChild(divcontainer);
+
+images.forEach((item) => {
+  const image = document.createElement("img");
+  image.src = item;
+  divcontainer.appendChild(image);
+});
+
 function newImg() {
-  image.src = images[currentImage];
+  divcontainer.style.transition = "transform 0.5s ease-in-out";
+  divcontainer.style.transform = `translateX(-${currentImage * 500}px)`;
 }
-container.appendChild(image);
-document.body.appendChild(container)
-
-
-/*
-.fade {
-  animation-name: fade;
-  animation-duration: 1.5s;
-}
-
-@keyframes fade {
-  from {opacity: .4} 
-  to {opacity: 1}
-} */
-// image.addEventListener("mouseover", (event) => {
-//   newImg()
-// });
-
-prevBtn.addEventListener("click", prevs)
 function prevs() {
+  if (isAnimating) return;
+  isAnimating = true;
+
   currentImage--;
   if (currentImage < 0) {
-    currentImage = images.length - 1;
-  }
-  newImg();
-};
-nextBtn.addEventListener("click", next )
-function next() {
-  currentImage++;
-  if (currentImage >= images.length) {
-    currentImage = 0;
+    currentImage = originalImages.length - 1;
   }
   newImg();
 }
-setInterval(next,3000)
+function next() {
+  if (isAnimating) return;
+  isAnimating = true;
+  currentImage++;
+  newImg();
+}
+divcontainer.addEventListener("transitionend", () => {
+  isAnimating = false;
+  if (currentImage === images.length - 1) {
+    divcontainer.style.transition = "none";
+    currentImage = 1;
+    divcontainer.style.transform = `translateX(-${currentImage * 500}px)`;
+  }
+    if (currentImage === 0) {
+      divcontainer.style.transition = "none";
+      currentImage = originalImages.length;
+      divcontainer.style.transform = `translateX(-${currentImage * 500}px)`;
+    }
+});
 newImg();
-
-
+function pause() {
+  interval = setInterval(next, 3000);
+}
+function nopause() {
+  if (interval !== undefined) {
+    clearInterval(interval);
+  }
+}
+pause();
+divcontainer.addEventListener("mouseover", nopause);
+divcontainer.addEventListener("mouseout", pause);
+if (nextBtn) {
+  nextBtn.addEventListener("click", next);
+}
+if (prevBtn) {
+  prevBtn.addEventListener("click", prevs);
+}
